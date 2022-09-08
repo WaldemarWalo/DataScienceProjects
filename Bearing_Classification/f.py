a655ca132af64c3ae61e49fa7b5fa3c5902d4a02
Waldemar Walo <waldemarwalo@gmail.com>
2022-09-08 09:33:48 +0200

    fixed ax sizing to 1080px wide

--- Bearing_Classification/f.py
+++ Bearing_Classification/f.py
@@ -8,12 +8,15 @@ import matplotlib.pyplot as plt
 def iqr(series):
     return series.quantile(0.75) - series.quantile(0.25)
 
-def get_ax(height=5, aspect=1):
-    return get_axes(1, 1, height=height, aspect=aspect)[0,0]
+def get_ax(size_ratio = 1, w_aspect = 1):
+    return get_axes(1, 1, size_ratio, w_aspect)[0,0]
 
-def get_axes(row_count=1, col_count=1, height=5, aspect=1) -> np.ndarray:
-    figsize = (col_count * height * aspect, row_count * height)
-    fig, axs = plt.subplots(row_count, col_count, figsize=figsize, layout='constrained');
+def get_axes(row_count=1, col_count=1, size_ratio = 1, w_aspect = 1) -> np.ndarray:
+    # max default kaggle's width resolution
+    max_width = 1080
+    fig_w = (max_width / 72 * size_ratio)
+    fig_h = ((row_count/col_count) * 1080 / 72 * size_ratio / w_aspect)
+    fig, axs = plt.subplots(row_count, col_count, figsize=(fig_w, fig_h), layout='constrained');
     
     # ensure we are returning an ndarray with 2d shape
     if not isinstance(axs, np.ndarray):
--- Bearing_Classification/transformations.py
+++ Bearing_Classification/transformations.py
@@ -2,6 +2,7 @@ version = '0.1'
 print(f'transformations loaded, version: {version}')
 
 import pandas as pd
+import numpy as np
 
 def get_iqr(series):
     return series.quantile(0.75) - series.quantile(0.25)
@@ -17,26 +18,30 @@ def drop_w_outliers(df: pd.DataFrame):
 def drop_idle_rpm(df: pd.DataFrame):
     return df.loc[df['rpm'] != 0]
 
-def plt_8_params_for_experiment(df, axs, exp_id, s=0.05): 
-    df = df[['rpm', 'w', 'a1_x', 'a1_y', 'a1_z', 'a2_x', 'a2_y', 'a2_z', 'timestamp']]
+def plt_8_params_for_experiment(df, axs, exp_id, s=0.01): 
+    df = df[['rpm', 'a1_x', 'a1_y', 'a1_z', 'w', 'a2_x', 'a2_y', 'a2_z', 'timestamp']]
     y_min_max_col = ['a1_x', 'a1_y', 'a1_z', 'a2_x', 'a2_y', 'a2_z']
     
+
+    
     for ax_i, (ax, col) in enumerate(zip(axs, df.columns[:len(axs)])):
-        y_min = df[y_min_max_col].min().min() * 0.95
-        y_max = df[y_min_max_col].max().max() * 1.05
         
         _ = ax.scatter(df['timestamp'], df[col], s=s)
         _ = ax.set_title(f'{exp_id} - {col}')
+        
         if col in y_min_max_col:
-            _ = ax.set_ylim(y_min, y_max)
+            y_min = df[y_min_max_col].min().min()
+            y_max = df[y_min_max_col].max().max()
         else:
-            y_min = df[col].min() * 0.95
-            y_max = df[col].max() * 1.05
-            _ = ax.set_ylim(y_min, y_max)
+            y_min = df[col].min()
+            y_max = df[col].max()
+            # ax.get_xaxis().set_ticks([])
+            # ax.get_yaxis().set_ticks([])
 
-# def plt_8_params(df, axs, s=0.25): 
-#     for ax_i, (ax_, col) in enumerate(zip(axs, df.columns[:len(axs)])):
-#         for cluster in sorted(df['rpm_clusters'].unique()):
-#             x = df.loc[df['rpm_clusters'] == cluster]
-#             _ = ax_.scatter(x['timestamp'], x[col], s=s)
-#         _ = ax_.set_title(f'{col}')
+        buffer= 0.1
+        y_max = y_max + buffer * np.abs(y_max)
+        y_min = y_min - buffer * np.abs(y_min)
+        _ = ax.set_ylim(y_min, y_max)
+        
+        # ax.axes.get_xaxis().set_visible(False)
+        # ax.axes.get_yaxis().set_visible(False)
