6cd8814bb34594490256a915cc4ec53259f6ab99
Waldemar Walo <waldemarwalo@gmail.com>
2022-09-17 10:52:20 +0200

    created toolkit - migrating UK cars

--- UK_Used_Car_Price_Regression/f.py
+++ UK_Used_Car_Price_Regression/f.py
@@ -1,46 +1,46 @@
-version = '0.2'
-print(f'framework loaded, version: {version}')
+# version = '0.2'
+# print(f'framework loaded, version: {version}')
 
-# import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+# # import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-def iqr(series):
-    return series.quantile(0.75) - series.quantile(0.25)
+# def iqr(series):
+#     return series.quantile(0.75) - series.quantile(0.25)
 
-def get_ax(h_aspect = 1, size_ratio = 1):
-    return get_axes(1, 1, h_aspect, size_ratio)[0,0]
+# def get_ax(h_aspect = 1, size_ratio = 1):
+#     return get_axes(1, 1, h_aspect, size_ratio)[0,0]
 
-def get_axes(row_count=1, col_count=1, h_aspect = 1, size_ratio = 1) -> np.ndarray:
-    # max default kaggle's width resolution
-    max_width = 1080
-    matplot_default_dpi = 72
-    fig_w = (max_width / matplot_default_dpi * size_ratio)
-    fig_h = ((row_count/col_count) * max_width / matplot_default_dpi * size_ratio * h_aspect)
-    # print((fig_w, fig_h))
-    fig, axs = plt.subplots(row_count, col_count, figsize=(fig_w, fig_h), layout='constrained');
+# def get_axes(row_count=1, col_count=1, h_aspect = 1, size_ratio = 1) -> np.ndarray:
+#     # max default kaggle's width resolution
+#     max_width = 1080
+#     matplot_default_dpi = 72
+#     fig_w = (max_width / matplot_default_dpi * size_ratio)
+#     fig_h = ((row_count/col_count) * max_width / matplot_default_dpi * size_ratio * h_aspect)
+#     # print((fig_w, fig_h))
+#     fig, axs = plt.subplots(row_count, col_count, figsize=(fig_w, fig_h), layout='constrained');
     
-    # ensure we are returning an ndarray with 2d shape
-    if not isinstance(axs, np.ndarray):
-        axs = np.array([axs])
-    if len(axs.shape) == 1:
-        axs = axs.reshape(len(axs), 1)
+#     # ensure we are returning an ndarray with 2d shape
+#     if not isinstance(axs, np.ndarray):
+#         axs = np.array([axs])
+#     if len(axs.shape) == 1:
+#         axs = axs.reshape(len(axs), 1)
         
-    return axs
+#     return axs
 
 
-import pickle
-from pathlib import Path
+# import pickle
+# from pathlib import Path
 
-def to_pkl(obj:any, file_name: str, overwrite:bool = False) -> None:
-    file_path = Path(file_name)
-    if file_path.exists() and not overwrite:
-        raise Exception(f'file already exits, set overwrite=True. File path {file_path}')
-    file_path.parents[0].mkdir(parents=True, exist_ok=True)
+# def to_pkl(obj:any, file_name: str, overwrite:bool = False) -> None:
+#     file_path = Path(file_name)
+#     if file_path.exists() and not overwrite:
+#         raise Exception(f'file already exits, set overwrite=True. File path {file_path}')
+#     file_path.parents[0].mkdir(parents=True, exist_ok=True)
     
-    with open(file_name, 'wb') as file:
-        pickle.dump(obj, file)
+#     with open(file_name, 'wb') as file:
+#         pickle.dump(obj, file)
         
-def from_pkl(file_name):
-    with open(file_name, 'rb') as file:
-        return pickle.load(file)
+# def from_pkl(file_name):
+#     with open(file_name, 'rb') as file:
+#         return pickle.load(file)
